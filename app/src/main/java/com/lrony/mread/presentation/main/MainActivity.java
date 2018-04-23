9f99f8f99201a92a20ab72d1de7be48281d874da
Lrony <5414115@qq.com>
2018-04-23 18:01:46 +0800

    [Main] bottombar init
    Change-Id: I8321ffab955e86ac5812c4971c4a570bf3922528

--- app/src/main/java/com/lrony/mread/presentation/main/MainActivity.java
+++ app/src/main/java/com/lrony/mread/presentation/main/MainActivity.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.lrony.mread.R;
 import com.lrony.mread.mvp.MvpActivity;
 
-public class MainActivity extends MvpActivity<MainContract.Presenter> implements MainContract.View {
+public class MainActivity extends MvpActivity<MainContract.Presenter> implements MainContract.View, BottomNavigationBar.OnTabSelectedListener {
 
     private static final String TAG = "MainActivity";
 
@@ -18,11 +19,44 @@ public class MainActivity extends MvpActivity<MainContract.Presenter> implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getPresenter().start();
+
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        mNavigationBar = findViewById(R.id.bottom_navigation_bar);
+        mNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_home, R.string.navigation_home))
+                .addItem(new BottomNavigationItem(R.drawable.ic_hot, R.string.navigation_search))
+                .addItem(new BottomNavigationItem(R.drawable.ic_list, R.string.navigation_library))
+                .addItem(new BottomNavigationItem(R.drawable.ic_self, R.string.navigation_self))
+                .setMode(BottomNavigationBar.MODE_FIXED)
+                .initialise();
+    }
+
+    private void initListener() {
+        mNavigationBar.setTabSelectedListener(this);
     }
 
     @NonNull
     @Override
     public MainContract.Presenter createPresenter() {
         return new MainPresenter();
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
     }
 }
