8a0dd33b70686e968da208b6d0f62daf69738ee7
Lrony <5414115@qq.com>
2018-04-23 16:39:03 +0800

    [APP] 重构
    Change-Id: I072189b1e18b2db81ab46e74c757db135b91cf21

--- app/src/main/java/com/lrony/mread/data/bean/BaseBean.java
+++
@@ -1,8 +0,0 @@
-package com.lrony.mread.data.bean;
-
-/**
- * Created by pengyang on 18-4-8.
- */
-public class BaseBean {
-    public boolean ok;
-}
---
+++ app/src/main/java/com/lrony/mread/presentation/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lrony.mread.presentation.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.lrony.mread.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
