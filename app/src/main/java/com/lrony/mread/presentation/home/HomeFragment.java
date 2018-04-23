9062bee581e5efad579153839d806d2a7e4c4660
Lrony <5414115@qq.com>
2018-04-23 22:38:57 +0800

    [Home] init

--- app/src/main/java/com/lrony/mread/presentation/home/HomeFragment.java
+++ app/src/main/java/com/lrony/mread/presentation/home/HomeFragment.java
@@ -3,6 +3,9 @@ package com.lrony.mread.presentation.home;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -12,9 +15,14 @@ import com.lrony.mread.mvp.MvpFragment;
 /**
  * Created by Lrony on 18-4-23.
  */
-public class HomeFragment extends MvpFragment<HomeContract.Presenter> implements HomeContract.View {
+public class HomeFragment extends MvpFragment<HomeContract.Presenter> implements HomeContract.View
+        , SwipeRefreshLayout.OnRefreshListener {
 
     private static final String TAG = "HomeFragment";
+
+    private Toolbar mToolbar;
+    private SwipeRefreshLayout mRefreshView;
+    private RecyclerView mRecyclerView;
 
     public static HomeFragment newInstance() {
         Bundle args = new Bundle();
@@ -46,9 +54,21 @@ public class HomeFragment extends MvpFragment<HomeContract.Presenter> implements
 
     private void initView(View view) {
         Log.d(TAG, "initView");
+        mToolbar = view.findViewById(R.id.home_toolbar);
+        mRefreshView = view.findViewById(R.id.home_refresh);
+        mRecyclerView = view.findViewById(R.id.home_recycler);
+
+        mToolbar.setTitle(getString(R.string.app_title));
+        mRefreshView.setColorSchemeResources(R.color.colorAccent);
     }
 
     private void initListener() {
         Log.d(TAG, "initListener");
+        mRefreshView.setOnRefreshListener(this);
+    }
+
+    @Override
+    public void onRefresh() {
+        mRefreshView.setRefreshing(false);
     }
 }
