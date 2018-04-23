2487d549bebf1f9b4a14a478b7b468c85e19f936
Lrony <5414115@qq.com>
2018-04-23 16:46:54 +0800

    [Main] mvp base
    Change-Id: I69e150ca9e8101e8becd8a6164561a5328e9840e

--- app/src/main/java/com/lrony/mread/presentation/main/MainActivity.java
+++ app/src/main/java/com/lrony/mread/presentation/main/MainActivity.java
@@ -1,12 +1,14 @@
 package com.lrony.mread.presentation.main;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.lrony.mread.R;
+import com.lrony.mread.mvp.MvpActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends MvpActivity<MainContract.Presenter> implements MainContract.View {
 
     private static final String TAG = "MainActivity";
 
@@ -16,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @NonNull
+    @Override
+    public MainContract.Presenter createPresenter() {
+        return new MainPresenter();
     }
 }
---
+++ app/src/main/java/com/lrony/mread/presentation/main/MainPresenter.java
@@ -0,0 +1,14 @@
+package com.lrony.mread.presentation.main;
+
+import com.lrony.mread.mvp.MvpBasePresenter;
+
+/**
+ * Created by Lrony on 18-4-23.
+ */
+public class MainPresenter extends MvpBasePresenter<MainContract.View> implements MainContract.Presenter {
+
+    @Override
+    public void start() {
+        super.start();
+    }
+}
