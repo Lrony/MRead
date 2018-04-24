b841ba057bf44bf9014668fd52ba8ac5fa6582ce
Lrony <5414115@qq.com>
2018-04-24 17:41:46 +0800

    [Search] init
    Change-Id: I2630c6b83820d7fcb7a47a261219a8fcfe249bed

--- app/src/main/java/com/lrony/mread/presentation/search/SearchFragment.java
+++ app/src/main/java/com/lrony/mread/presentation/search/SearchFragment.java
@@ -3,18 +3,26 @@ package com.lrony.mread.presentation.search;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 
 import com.lrony.mread.R;
 import com.lrony.mread.mvp.MvpFragment;
 
+import me.zhanghai.android.materialprogressbar.MaterialProgressBar;
+
 /**
  * Created by Lrony on 18-4-23.
  */
-public class SearchFragment extends MvpFragment<SearchContract.Presenter> implements SearchContract.View {
+public class SearchFragment extends MvpFragment<SearchContract.Presenter> implements SearchContract.View, View.OnClickListener {
 
     private static final String TAG = "SearchFragment";
+
+    private ViewPager mViewPager;
+    private TabLayout mTab;
+    private MaterialProgressBar mProgress;
 
     public static SearchFragment newInstance() {
         Bundle args = new Bundle();
@@ -41,14 +49,33 @@ public class SearchFragment extends MvpFragment<SearchContract.Presenter> implem
         getPresenter().start();
 
         initView(view);
-        initListener();
+        initListener(view);
     }
 
     private void initView(View view) {
         Log.d(TAG, "initView");
+        mViewPager = view.findViewById(R.id.vp_search);
+        mTab = view.findViewById(R.id.tab_search);
+        mProgress = view.findViewById(R.id.progress);
     }
 
-    private void initListener() {
+    private void initListener(View view) {
         Log.d(TAG, "initListener");
+        view.findViewById(R.id.view_search).setOnClickListener(this);
+        view.findViewById(R.id.fl_action).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.view_search:
+                Log.d(TAG, "onClick: Search");
+                showToast("Search");
+                break;
+            case R.id.fl_action:
+                Log.d(TAG, "onClick: Action");
+                showToast("Action");
+                break;
+        }
     }
 }
