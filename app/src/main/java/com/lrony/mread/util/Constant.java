a35e13a96820512d93ff8bb6d942a8f6f498d244
Lrony <5414115@qq.com>
2018-04-25 17:52:33 +0800

    [APP] modify recycler_status_view layout
    Change-Id: Iebb4d5e2e69df7abb91c127be97d0f1c9d85508b

--- app/src/main/java/com/lrony/mread/util/Constant.java
+++ app/src/main/java/com/lrony/mread/util/Constant.java
@@ -5,6 +5,8 @@ package com.lrony.mread.util;
  */
 public class Constant {
 
+    public static final String URL_BAIDU = "http://www.baidu.com";
+
     /*URL_BASE*/
     public static final String API_BASE_URL = "http://api.zhuishushenqi.com";
     public static final String IMG_BASE_URL = "http://statics.zhuishushenqi.com";
