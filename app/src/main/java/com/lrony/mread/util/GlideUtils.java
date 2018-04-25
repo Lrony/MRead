5474015ba0feeacd7afc34d259b87747398768cb
Lrony <5414115@qq.com>
2018-04-25 20:35:27 +0800

    [Util] add
    Change-Id: I21ee100e2936d6a685c2cd08047b50e2523b8c1f

---
+++ app/src/main/java/com/lrony/mread/util/GlideUtils.java
@@ -0,0 +1,23 @@
+package com.lrony.mread.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.lrony.mread.ui.help.GlideApp;
+
+import java.io.File;
+
+/**
+ * Created by Lrony on 18-4-25.
+ */
+public class GlideUtils {
+
+    public static void downLoadImage(Context context, String url) {
+
+    }
+}
