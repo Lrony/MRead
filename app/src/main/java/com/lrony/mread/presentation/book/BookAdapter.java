b3b3e6fdf28b4f50be6b66241f5822ea58c0a525
Lrony <5414115@qq.com>
2018-04-25 13:40:06 +0800

    [Search] item
    Change-Id: I0b9b89069574e075eea3d613882a02f318a2b769

--- app/src/main/java/com/lrony/mread/presentation/book/BookAdapter.java
+++ app/src/main/java/com/lrony/mread/presentation/book/BookAdapter.java
@@ -3,12 +3,14 @@ package com.lrony.mread.presentation.book;
 import android.content.Context;
 import android.widget.ImageView;
 
+import com.lrony.mread.App;
 import com.lrony.mread.R;
 import com.lrony.mread.data.bean.Book;
 import com.lrony.mread.ui.help.CommonAdapter;
 import com.lrony.mread.ui.help.CommonViewHolder;
 import com.lrony.mread.ui.help.GlideApp;
 import com.lrony.mread.util.Constant;
+import com.lrony.mread.util.StringUtils;
 
 import java.util.List;
 
@@ -30,6 +32,8 @@ public class BookAdapter extends CommonAdapter<Book> {
         helper.setText(R.id.tv_describe, item.getShortIntro());
         helper.setText(R.id.tv_author, item.getAuthor());
         helper.setText(R.id.tv_type, item.getMinorCate());
+        helper.setText(R.id.tv_retention_ratio, item.getRetentionRatio()
+                + StringUtils.getStringByID(R.string.book_retention_ratio));
 
         GlideApp.with(context)
                 .load(Constant.IMG_BASE_URL + item.getCover())
