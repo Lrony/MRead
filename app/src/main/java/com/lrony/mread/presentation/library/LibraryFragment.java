87619343470550ba5d5a4ebe77da44360449ac52
Lrony <5414115@qq.com>
2018-05-04 14:36:03 +0800

    [Library] push
    Change-Id: Iba4eecb4a7dc064d8b7f85c43e21c1818afb061a

--- app/src/main/java/com/lrony/mread/presentation/library/LibraryFragment.java
+++ app/src/main/java/com/lrony/mread/presentation/library/LibraryFragment.java
@@ -1,11 +1,8 @@
 package com.lrony.mread.presentation.library;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
